3154d7f15d50378a6ca3e96405ae201002ddba22
Sangam Basnet <sangambasnet1800@gmail.com>
2025-01-17 11:59:04 +0545

    hive model for login

---
+++ lib/app/constants/hive_table_constatnts.dart
@@ -0,0 +1,12 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  static const int loginTableId = 0;
+  static const String loginBox = 'loginBox';
+
+  static const int signUpTableId = 1;
+  static const String signUpBox = 'signUpBox';
+
+  static const int userTableId = 2;
+  static const String userBox = 'userBox';
+}
